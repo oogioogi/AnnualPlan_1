1807ef6ac2a8cf17a927d699a54a30d0b54d4ddc
oogioogi <detacl73@gmail.com>
2021-10-24 06:32:12 +0900

    Initial Commit

---
+++ AnnualPlan/AnnualPlanApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnnualPlanApp.swift
+//  AnnualPlan
+//
+//  Created by 이용석 on 2021/10/24.
+//
+
+import SwiftUI
+
+@main
+struct AnnualPlanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnnualPlan/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AnnualPlan
+//
+//  Created by 이용석 on 2021/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
