866e9cc597aa7d331f0dc4ed5bdc14a7c9f00244
oogioogi <detacl73@gmail.com>
2021-10-29 16:19:41 +0900

    - alert Box 수정

--- AnnualPlan/Views/Calendar/AlertAddTaskView.swift
+++ AnnualPlan/Views/Calendar/AlertAddTaskView.swift
@@ -15,73 +15,119 @@ struct AlertAddTaskView: View {
     
     var vacation: [String] = ["연차", "월차"]
     var body: some View {
-        VStack{
-            HStack(spacing: 20) {
+        
+        VStack {
+            topTiileView() // top View
+            middleView()   // middle View
+            bottomView()   // bottom View
+        }
+        .frame(maxWidth: UIScreen.main.bounds.width * 0.5)
+        .padding()
+        .background(Color.yellow)
+        .cornerRadius(20)
+        .shadow(color: .gray, radius: 3, x: 3, y: 3)
+    }
+    
+@ViewBuilder
+    // top View
+    func topTiileView() -> some View {
+        ZStack(alignment: .center) {
+            GeometryReader { geometry in
                 Text(currentDate.toString(dateFormat: "YYYY/MM/dd"))
-                    .font(.title2)
-                    .bold()
-            }.padding(.horizontal)
-            
-            VStack{
-                if let taskDetail = taskClass.tasks.first(where: { detail in
-                    shared.isSameDay(date1: detail.taskDate, date2: currentDate)
-                }) {
-                    ForEach(taskDetail.task) { task in
-                        VStack(alignment: .center, spacing: 10) {
-                            Text(task.title)
-                                .font(.caption).bold()
-                        }
-                        .padding(.vertical, 10)
-                        //.padding(.horizontal)
-                        .frame(maxWidth: .infinity, alignment: .center)
-                        .background(
-                            Color.purple
-                                .opacity(0.5)
-                                .cornerRadius(10)
-                        )
-                    }
-                }else {
-                    Text("Task Not Found")
-                        .padding(.vertical, 10)
-                        .frame(maxWidth: .infinity, alignment: .center)
-                        .background(
-                            Color.purple
-                                .opacity(0.5)
-                                .cornerRadius(10)
-                        )
-                }
-                
-                
-//                Toggle(isOn: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Is On@*/.constant(true)/*@END_MENU_TOKEN@*/) {
-//                    Text(vacation[0]).bold()
-//                }
-//                Toggle(isOn: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Is On@*/.constant(true)/*@END_MENU_TOKEN@*/) {
-//                    Text(vacation[1]).bold()
-//                }
-            }.padding()
-            
-            HStack(spacing: 30) {
-                Button(action: {
-                    self.isAlert.toggle()
-                }, label: {
-                    Image(systemName: "checkmark.circle.fill")
-                        .font(.title)
-                        .foregroundColor(.primary)
-                })
+                    .frame(maxWidth: .infinity)
+                    .font(.system(size: 20, weight: .bold, design: .rounded))
                 Button(action: {
                     self.isAlert.toggle()
                 }, label: {
                     Image(systemName: "xmark.circle.fill")
-                        .font(.title)
-                        .foregroundColor(.primary)
-                })
+                        .foregroundColor(.purple)
+                        .font(.title2)
+                }).offset(x: geometry.size.width, y: -(geometry.size.height * 0.5))
             }
+
         }
-        .frame(maxWidth: UIScreen.main.bounds.width * 0.5)
-        .padding()
-        .background(Color.gray)
-        .cornerRadius(20)
-        .shadow(color: .black, radius: 5, x: 5, y: 5)
+        .frame(height: 25)
+        .padding(.horizontal)
+    }
+    // middle View
+    func middleView() -> some View {
+        VStack{
+            if let taskDetail = taskClass.tasks.first(where: { detail in
+                shared.isSameDay(date1: detail.taskDate, date2: currentDate)
+            }) {
+                ForEach(taskDetail.task) { task in
+                    VStack(alignment: .center, spacing: 10) {
+                        Text(task.title)
+                            .font(.caption).bold()
+                    }
+                    .padding(.vertical, 10)
+                    .frame(maxWidth: .infinity, alignment: .center)
+                    .background(
+                        Color.purple
+                            .opacity(0.5)
+                            .cornerRadius(10)
+                    )
+                    .onTapGesture {
+                        debugPrint("clicked tap")
+                    }
+                }
+            }else {
+                Text("Task Not Found")
+                    .padding(.vertical, 10)
+                    .frame(maxWidth: .infinity, alignment: .center)
+                    .background(
+                        Color.purple
+                            .opacity(0.5)
+                            .cornerRadius(10)
+                    )
+            }
+        }.padding()
+    }
+    // bottom View
+    func bottomView() -> some View {
+        HStack(spacing: 30) {
+            // task appending
+            Button(action: {
+                if let appendTaskIndex = taskClass.tasks.firstIndex(where: { tasked in
+                    tasked.taskDate == currentDate
+                }) {
+                    debugPrint(appendTaskIndex)
+                    debugPrint(currentDate)
+                    taskClass.tasks[appendTaskIndex].task.append(Task(title: "New Annul"))
+                   
+                }else {
+                    debugPrint(currentDate)
+                    taskClass.tasks.append(TaskDetail(task: [Task(title: "Annul")], taskDate: currentDate))
+                }
+                //동일 날짜는 Task만을 추가 한다
+            }, label: {
+                Image(systemName: "plus.circle.fill")
+                    .font(.title)
+                    .foregroundColor(.primary)
+            })
+            
+            // task delete
+            Button(action: {
+                let removeTaskIndex = taskClass.tasks.firstIndex { tasked in
+                    tasked.taskDate == currentDate
+                }
+
+                if removeTaskIndex != nil {
+                    debugPrint(removeTaskIndex?.description as Any)
+                    let taskCount = taskClass.tasks[removeTaskIndex!].task.count
+                    if taskCount > 1 {
+                        taskClass.tasks[removeTaskIndex!].task.remove(at: taskCount - 1)
+                    }else {
+                        taskClass.tasks.remove(at: removeTaskIndex!)
+                    }
+                    
+                }
+            }, label: {
+                Image(systemName: "minus.circle.fill")
+                    .font(.title)
+                    .foregroundColor(.primary)
+            })
+        }
     }
 }
 
